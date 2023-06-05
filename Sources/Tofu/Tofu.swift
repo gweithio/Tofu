b16b1b7e21a236a33e40f6a1b47b4e97935ed5ca
ethan <ethan@gweithio.com>
2023-06-05 11:14:39 +0100

    make init public

--- Sources/Tofu/Tofu.swift
+++ Sources/Tofu/Tofu.swift
@@ -3,7 +3,7 @@ import Foundation
 open class Tofu {
   private var loggerLevel: TofuLevel
 
-  init(level: TofuLevel = TofuLevel.Debug) { self.loggerLevel = level }
+  public init(level: TofuLevel = TofuLevel.Debug) { self.loggerLevel = level }
 
   public func setLevel(to level: TofuLevel) {
     loggerLevel = level
@@ -13,27 +13,42 @@ open class Tofu {
     return lhs.loggerLevel == rhs.loggerLevel
   }
 
-  open func logFormat(level: TofuLevel, _ message: @autoclosure @escaping () -> String, file: String = #file, function: String = #function, line: UInt = #line) {
+  open func logFormat(
+    level: TofuLevel, _ message: @autoclosure @escaping () -> String, file: String = #file,
+    function: String = #function, line: UInt = #line
+  ) {
     print("[\(level.rawValue)] \(function):\(line) \(message())")
   }
 
   @inlinable
-  open func err(_ message: @autoclosure @escaping () -> String, file: String = #file, function: String = #function, line: UInt = #line) {
+  open func err(
+    _ message: @autoclosure @escaping () -> String, file: String = #file,
+    function: String = #function, line: UInt = #line
+  ) {
     logFormat(level: TofuLevel.Err, message())
   }
 
   @inlinable
-  open func warn(_ message: @autoclosure @escaping () -> String, file: String = #file, function: String = #function, line: UInt = #line) {
+  open func warn(
+    _ message: @autoclosure @escaping () -> String, file: String = #file,
+    function: String = #function, line: UInt = #line
+  ) {
     logFormat(level: TofuLevel.Warn, message())
   }
 
   @inlinable
-  open func debug(_ message: @autoclosure @escaping () -> String, file: String = #file, function: String = #function, line: UInt = #line) {
+  open func debug(
+    _ message: @autoclosure @escaping () -> String, file: String = #file,
+    function: String = #function, line: UInt = #line
+  ) {
     logFormat(level: TofuLevel.Debug, message())
   }
 
   @inlinable
-  open func log(level: TofuLevel, _ message: @autoclosure @escaping () -> String, file: String = #file, function: String = #function, line: UInt = #line) {
+  open func log(
+    level: TofuLevel, _ message: @autoclosure @escaping () -> String, file: String = #file,
+    function: String = #function, line: UInt = #line
+  ) {
     logFormat(level: TofuLevel.Log, message())
   }
 }
